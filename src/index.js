4cbd5d777f39133c7e9f930d77d22fe374383b25
Debian <bitnami@ip-172-26-7-135.us-west-2.compute.internal>
2021-10-26 12:37:32 +0000

    Setup MongoDb with Mongoose in Express: folder structure: src/models src/routes; in models/user and message STILL BUGGYgit status

--- src/index.js
+++ src/index.js
@@ -3,9 +3,10 @@ import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
 import { v4 as uuidv4 } from 'uuid';
-import models from './models';
-
+import routes from './routes';
 const port = process.env.PORT || 8888
+import models, { connectDb } from './models';
+
 // Create the express app
 const app = express()
 
@@ -16,21 +17,14 @@ const app = express()
 app.use(express.json());
 app.use(cors());
 app.use(express.urlencoded({extended:true  }));
-app.use( (req, res, next) => {
-	req.context = {
-		models,
-		me: models.users[1],
-	};
-	next();
-});
+app.use('/session', routes.session);
+app.use('/users', routes.user);
+app.use('/messages', routes.message);
 // app.get(/* ... */)
 
 
-app.get('/session', (req, res) => {
-	return res.send(req.context.models.users[req.context.me.id]);
-});
 app.get('/', (req, res) => {
-   res.send('Received a GET HTTP method');
+   res.send('Received a GET GET GET GETGET HTTP method');
 })
 app.post('/', (req, res) => {
    return res.send('Received a POST HTTP method');
@@ -42,50 +36,11 @@ app.delete('/', (req, res) => {
    return res.send('Received a DELETE HTTP method');
 })
 
-app.get('/users', (req, res) => {
-//   res.send('Received a GET HTTP method on user resource');
-     return res.send(Object.values(req.context.models.users));
-});
 
-app.get('/users/:userId', (req, res) => {
-     return res.send(req.context.models.users[req.params.userId]);
-})
 
-app.get('/messages', (req, res) => {
-     return res.send(Object.values(req.context.models.messages));
-})
-app.get('/messages/:messageId', (req, res) => {
-     return res.send(req.context.models.messages[req.params.messageId]);
-})
-app.post('/users', (req, res) => {
-   return res.send('Received a POST HTTP method on user resource');
-})
-app.post('/messages', (req, res) => {
-	const id = uuidv4();
-	console.log(id)
-	const message = {
-		id,
-		text: req.body.text,
-		userId: req.context.me.id,
-	};
-	console.log(`Added: text: ${message.text} - userId: ${message.userId}`)
-	req.context.models.messages[id] = message; //pseudodatabase
-	return res.send(`message: ${message.text}`);
-});
 app.put('/users/:userId', (req, res) => {
    return res.send(`Received a PUT HTTP method on user/${req.params.userId} resource`);
 })
-app.delete('/users/:userId', (req, res) => {
-   return res.send(`Received a DELETE HTTP method on user/${req.params.userId}`);
-});
-app.delete('/messages/:messageId', (req, res) => {
-	const {
-		[req.params.messageId]: message,
-		... otherMessages
-	} = req.context.models.messages;
-	req.context.models.messages = otherMessages;
-	return res.send(`deleted: ${message}`);
-});
 // Error handlers
 app.use(function fourOhFourHandler (req, res) {
   res.status(404).send()
@@ -96,10 +51,41 @@ app.use(function fiveHundredHandler (err, req, res, next) {
 })
 
 // Start server
-app.listen(port, function (err) {
-  if (err) {
-    return console.error(err)
-  }
+connectDb().then(async() => {
+	app.listen(port, function (err) {
+  		if (err) {
+    			return console.error(err)
+  		}
 
-  console.log(`Started at http://localhost:${port}`)
-})
+  		console.log(`Started at http://localhost:${port}`)
+	})
+});
+
+const createUserWithMessages = async () => {
+	console.log(`create user1 and message1`);
+	const user1 = new models.User({
+		username: 'rwieruch',
+	});
+	const message1 = new models.Message({
+		text: 'Published the Road to Learn React',
+		user: user1.id,
+	});
+	await message1.save();
+	await user1.save();
+};
+
+
+/*
+const eraseDbOnSync = true;
+connectDb().then(async () => {
+	if (eraseDbOnSync) {
+		await Promise.all([
+			models.User.deleteMany({}),
+			models.Message.deleteMany({}),
+			]);
+	}
+	app.listen(port, () => {
+		console.log(`Listening on port ${port}!`);
+	});
+});  // To reinitialize Db after rebooting express server
+*/
